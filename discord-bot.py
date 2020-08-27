b0fdb011d34bb0360e3631e9cf88350832e879f3
harrisbaker99 <harrisbaker02@outlook.com>
2020-08-27 18:18:45 +1000

    Change Role Colour Function Added

--- discord-bot.py
+++ discord-bot.py
@@ -22,9 +22,7 @@ async def change_status():
 
     while not bot.is_closed():
         status = next(statuses)
-
         await bot.change_presence(activity=discord.Game(status))
-
         await asyncio.sleep(10)
 
 
@@ -32,10 +30,13 @@ async def change_nick(member: discord.Member, nick):
     await member.edit(nick=nick)
 
 
+async def change_role_colour_function(role: discord.Role, r, g, b):
+    await role.edit(colour=discord.Colour.from_rgb(r, g, b))
+
+
 async def change_jayden():
     await bot.wait_until_ready()
     nicknames = cycle(['Big Dickhead', 'CockSucker', 'Fucktard', 'Wankstick', 'Retard', 'Spastic Cunt'])
-
     jayden_id = bot.get_user(142933221015945216)
 
     for guild in bot.guilds:
@@ -45,31 +46,53 @@ async def change_jayden():
 
     while not bot.is_closed():
         nickname = next(nicknames)
-
         await change_nick(jayden_name, nickname)
-
         await asyncio.sleep(43200)
 
 
 async def change_harris():
     await bot.wait_until_ready()
     nicknames = cycle(['Commander Cock', 'Cum Master', 'Cum Cum', 'Cum Slut', 'PurePhoenix'])
-
     harris_id = bot.get_user(230854079847464960)
+    guild_id = bot.get_guild(368541462188654592)
 
     for guild in bot.guilds:
-        for member in guild.members:
-            if member == harris_id:
-                harris_name = member
+        if guild == guild_id:
+            for member in guild.members:
+                if member == harris_id:
+                    harris_name = member
 
     while not bot.is_closed():
         nickname = next(nicknames)
-
         await change_nick(harris_name, nickname)
-
         await asyncio.sleep(20)
 
 
+async def change_role_colour():
+    await bot.wait_until_ready()
+
+    guild_id = bot.get_guild(368541462188654592)
+
+    r_colours = cycle([255, 255, 255, 155, 0, 0, 0, 0, 0, 155, 255, 255])
+    g_colours = cycle([0, 155, 255, 255, 255, 255, 255, 155, 0, 0, 0, 0])
+    b_colours = cycle([0, 0, 0, 0, 0, 155, 255, 255, 255, 255, 255, 155])
+
+    for guild in bot.guilds:
+        if guild == guild_id:
+            role = discord.utils.get(guild_id.roles, name="Homos")
+            print(role)
+
+    while not bot.is_closed():
+        r_colour = int(next(r_colours))
+        g_colour = int(next(g_colours))
+        b_colour = int(next(b_colours))
+
+        # print('r: ' + str(r_colour) + ' ' + 'g: ' + str(g_colour) + ' ' + 'b: ' + str(b_colour))
+        await change_role_colour_function(role, r_colour, g_colour, b_colour)
+        await asyncio.sleep(10)
+
+
 bot.loop.create_task(change_jayden())
+bot.loop.create_task(change_harris())
 bot.loop.create_task(change_status())
 bot.run(token)
