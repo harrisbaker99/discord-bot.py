3a92fffdcf32077a72ce4bf32b93a2a5a2e8d55e
harrisbaker99 <harrisbaker02@outlook.com>
2021-01-16 11:24:27 +1100

    Added Cumrag sound command

--- cogs/sound.py
+++ cogs/sound.py
@@ -123,7 +123,7 @@ class SoundClips(commands.Cog):
                 voice = await channel.connect()
             voice.play(discord.FFmpegPCMAudio(audio), after=lambda: print(f"Playing {audio}"))
             voice.source = discord.PCMVolumeTransformer(voice.source)
-            voice.source.volume = 0.2
+            voice.source.volume = 0.7
             await asyncio.sleep(int(3))
             voice.stop()
             await voice.disconnect()
